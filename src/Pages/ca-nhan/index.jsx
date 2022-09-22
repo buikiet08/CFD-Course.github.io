fcc443e9b26e108d13c687e8daa71708fc27e0c0
buikiet08 <89330660+buikiet08@users.noreply.github.com>
2022-09-22 23:59:23 +0700

    update info

--- src/Pages/ca-nhan/index.jsx
+++ src/Pages/ca-nhan/index.jsx
@@ -1,19 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
+import { usePage } from '../../hooks/usePage'
 
 function Profile() {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const [data, setData] = useState({})
+    const { user } = usePage()
+    const [isFetching, setIsFetching] = useState(false)
+    const [errorMessage, setErrorMessage] = useState('')
+    const onSubmit = async () => {
+        setIsFetching(true)
+        try {
+            const updateInfo = await userService.updateInfo(data)
+            if (updateInfo.data) {
+                localStorage.setItem('token', JSON.stringify(updateInfo.data))
+                localStorage.setItem('user', JSON.stringify(updateInfo.data))
+            }
+        }
+        catch(error) {
+            setErrorMessage(error.message)
+        }
+        finally {
+            setIsFetching(false)
+        }
+        console.log(data)
+    }
+    console.log(user)
     return (
-        <div className="tab1">
+        <form className="tab1" onSubmit={handleSubmit(onSubmit)}>
             <label>
                 <p>Họ và tên<span>*</span></p>
-                <input type="text" placeholder="Nguyễn Văn A" />
+                <input onChange={(e) => data.name = e.target.value} type="text" placeholder={user.name} />
             </label>
             <label>
                 <p>Số điện thoại<span>*</span></p>
-                <input type="text" placeholder="0949******" />
+                <input type="text" onChange={(e) => data.phone = e.target.value} placeholder="0949******" />
             </label>
             <label>
                 <p>Email<span>*</span></p>
-                <input defaultValue="vuong.dang@dna.vn" disabled type="text" />
+                <input defaultValue={user.email} disabled type="text" />
             </label>
             <label>
                 <p>Facebook<span>*</span></p>
@@ -23,8 +48,8 @@ function Profile() {
                 <p>Skype<span>*</span></p>
                 <input type="text" placeholder="Skype url" />
             </label>
-            <div className="btn main rect">LƯU LẠI</div>
-        </div>
+            <button style={{ border: 'none', outline: 'none' }} className="btn main rect">LƯU LẠI</button>
+        </form>
     )
 }
 
